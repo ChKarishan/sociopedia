908a154fcf081b4d70ac0bf98ce5c87c7a7e04a8
ChKarishan <karishan.897@gmail.com>
2023-08-27 02:43:08 +0500

    authentication and authorization

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import helmet from "helmet"
 import morgan from "morgan";
 import path from "path"
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
+import authRoutes from "./routes/auth.js";
 
 /* CONFIGURATIONS */
 
@@ -38,12 +40,19 @@ const storage = multer.diskStorage({
 });
 const upload = multer({storage});
 
+/* Routes With Files */
+app.post("/auth/register", upload.single("picture"), register);
+
+
+/* Routes */
+app.use("/auth", authRoutes);
+
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParsers: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
-    app.listen(PORT, () => console.log(" Server Port: ${PORT}"));
-}).catch((error) => console.log(" ${error} did not conenct"));
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not conenct`));
