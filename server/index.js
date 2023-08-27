92fd9e9218f0ce0e854e867deadaaea17fe0e503
Karishan Mehmood <88370483+ChKarishan@users.noreply.github.com>
2023-08-28 01:19:41 +0500

    postRoutes Setup

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js"
+import {verifyToken} from "./middleware/auth.js"    
+import { createPost } from "./controllers/posts.js"
 
 /* CONFIGURATIONS */
 
@@ -43,11 +46,13 @@ const upload = multer({storage});
 
 /* Routes With Files */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 
 /* Routes */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postsRoutes);
 
 
 /* MONGOOSE SETUP */
